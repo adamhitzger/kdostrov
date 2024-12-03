3d4b54071418bc4796e1f16537936b1c6fa08045
adamhitzger <adam.hitzger@icloud.com>
2024-12-03 18:19:30 +0100

    360° prohlidka

--- app/fotogalerie/page.tsx
+++ app/fotogalerie/page.tsx
@@ -23,7 +23,14 @@ export default async function Gallery() {
                     <h1 className="text-4xl font-bold text-white">Ukázky sálu</h1>
                 </div>
             </div>
-            <div className={`my-10 grid grid-cols-2 lg:grid-cols-3 w-full gap-5 lg:gap-8 ${ukazka.length > 0 ? "min-h-screen" : "h-5"} `}>
+            <div className={`my-10 grid grid-cols-2 lg:grid-cols-3 w-full gap-5 lg:gap-8 ${ukazka.length > 0 ? "h-auto" : "h-5"} `}>
+            <div>
+            <iframe src="https://kuula.co/share/collection/7J9Ms?logo=1&card=1&info=0&logosize=40&fs=1&vr=1&zoom=1&initload=0&thumbs=1&inst=0" className="w-full h-96">
+            </iframe>
+            <div className="text-white flex flex-col h-1/3 text-lg p-2">
+                            <span>360° prohlídka</span>
+                            </div>
+            </div>
                 {ukazka.map((g: GalleryCard, idx: number) => (
 
                     <div key={idx} className="w-full h-full ">
